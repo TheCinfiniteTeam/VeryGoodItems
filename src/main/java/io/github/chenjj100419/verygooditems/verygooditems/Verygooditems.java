5ea9713dc860acbdaf0ed563cbb8f3efebd092e9
chenjj100419 <61618153+chenjj100419@users.noreply.github.com>
2020-09-06 13:43:10 +0800

    updatefile

--- src/main/java/io/github/chenjj100419/verygooditems/verygooditems/Verygooditems.java
+++ src/main/java/io/github/chenjj100419/verygooditems/verygooditems/Verygooditems.java
@@ -21,7 +21,7 @@ public class Verygooditems {
 
     public static final String MOD_ID = "verygooditems";
     public static final String MOD_NAME = "Verygooditems";
-    public static final String VERSION = "1.0-SNAPSHOT";
+    public static final String VERSION = "1.0.0";
 
     /**
      * This is the instance of your mod as created by Forge. It will never be null.
